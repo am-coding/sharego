cc8983ee03afa5b196b336c40edce94ff5344b32
am-coding <amrikdeveloper@gmail.com>
2020-08-19 22:39:50 +0530

    some cleanup

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,63 +1,23 @@
 const express = require('express')
 const router = express.Router();
-const { BlobServiceClient } = require("@azure/storage-blob");
 const Busboy = require('busboy');
-const { v4: uuidv4 } = require('uuid');
+const blobStorage = require('../services/blobStorage');
 
+router.post('/upload', function(req, res) {
 
-router.post("/am", (req, res) => {
-  const busboy = new Busboy({ headers: req.headers });
+  var busboy = new Busboy({ headers: req.headers });
 
   busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-      console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-      file.on('data', function(data) {
-       
-        async function main() {
-          console.log('Azure Blob storage v12 - JavaScript quickstart sample');
-          const connectionString = process.env.CONNECTION_STRING
-          // Create the BlobServiceClient object which will be used to create a container client
-          const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
-
-          // Create a unique name for the container
-          const containerName = 'quickstart' + uuidv4();
-
-          console.log('\nCreating container...');
-          console.log('\t', containerName);
-
-          // Get a reference to a container
-          const containerClient = blobServiceClient.getContainerClient(containerName);
-
-          // Create the container
-          const createContainerResponse = await containerClient.create();
-          console.log("Container was created successfully. requestId: ", createContainerResponse.requestId);
-
-        // Create a unique name for the blob
-          const blobName = 'quickstart' + uuidv4() + '.jpg';
-
-          // Get a block blob client
-          const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-
-          console.log('\nUploading to Azure storage as blob:\n\t', blobName);
-
-          // Upload data to the blob
-          //const data = require('./test.jpg')
-          const uploadBlobResponse = await blockBlobClient.uploadFile(data);
-          console.log("Blob was uploaded successfully. requestId: ", uploadBlobResponse.requestId);
-
-          console.log('\nListing blobs...');
-
-          // List the blob(s) in the container.
-          for await (const blob of containerClient.listBlobsFlat()) {
-              console.log('\t', blob.name);
-          }
-
-        }
-        main();
+      blobStorage.saveToBlob(filename, file, function(err, result){
+      if(err) {
+          res.setEncoding(500, err);
+      } else {
+          res.redirect('/show?name=' + encodeURI(filename));
+      }
       });
-    });
-
-
+  });
   req.pipe(busboy);
-})
+  });
+
 
 module.exports = router;
